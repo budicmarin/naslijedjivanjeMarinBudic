c6dc7add8a174a79b322b0a83957278cc39065e2
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-10-13 10:15:59 +0200

    Add project files.

---
+++ ListBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace naslijedjivanjeMarinBudic
+{
+    class ListBox:IControl
+    {
+        public void Metoda()
+        {
+            string output;
+            output = "Naziv Metode je Metoda";
+            Console.WriteLine(output);
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naslijedjivanjeMarinBudic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListBox x = new ListBox();
+            x.Metoda();
+            ComboBox y = new ComboBox();
+            y.Metoda();
+            Console.ReadKey();
+
+        }
+    }
+}
